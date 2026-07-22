23cdbcb1c2bd112ead0c8e8bd563b156199ff010
codingwithrahat <codingwithrahat@gmail.com>
2026-07-22 16:29:38 +0600

    update

--- Product Shop/src/main/java/com/turtlesltd/productshop/ProductController.java
+++ Product Shop/src/main/java/com/turtlesltd/productshop/ProductController.java
@@ -9,11 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
+    private final List<Product> products = new ArrayList<>();
 
     @GetMapping("/add")
     public String showForm(Model model){
@@ -30,6 +36,37 @@ public class ProductController {
             return "form";
         }
 
+        products.add(product);
+        log.info("Product {} has been saved", product);
+
         return "redirect:/product/add";
     }
+
+
+
+
+    //list
+
+    @GetMapping("/list")
+    public String list(Model model){
+        model.addAttribute("products", products);
+        return "list";
+    }
+
+
+
+
+    @GetMapping("/remove/{id}")
+    public String remove(@PathVariable String id) {
+
+        products.removeIf(product -> product.getId().equals(id));
+
+        log.info("Product with ID {} removed", id);
+
+        return "redirect:/product/list";
+    }
+
+
+    
+
 }
